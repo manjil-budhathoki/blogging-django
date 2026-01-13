b3b7e51690633a4ee32675758c5fcd6a21ff5708
Navya <manjilbbudhathoki@gmail.com>
2026-01-13 14:32:47 +0545

    Basic Blogging app

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
+from .models import Post
 # Create your views here.
+def post_list(request):
+    post = Post.published.all()
+    return render(
+        request,
+        'blog/post/list.html',
+        {'posts': post}
+    )
+
+def post_detail(request, id):
+    '''
+        try:
+            post = Post.published.get(id=id)
+        except Post.DoesNotExist:
+            raise Http404("Post does not exist")
+    '''
+    
+    post = get_object_or_404(
+        Post,
+        id = id,
+        status = Post.Status.PUBLISHED
+    )
+
+    return render(
+        request,
+        'blog/post/detail.html',
+        {'post': post}
+    )
