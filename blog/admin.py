2f82d460cede8bf1fd8d7c020279b22ebbbf5af5
Navya <manjilbbudhathoki@gmail.com>
2026-01-13 12:31:11 +0545

    Adding the initial  upto the custom managers

---
+++ blog/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from .models import Post
+# Register your models here.
+
+# admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'author', 'published_date', 'status']
+    list_filter = ['status', 'created_date', 'published_date', 'author']
+    search_fields = ['title', 'body']
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ['author']
+    date_hierarchy = 'published_date'
+    ordering = ['status', 'published_date']
+    show_facets = admin.ShowFacets.ALWAYS
+    
---
+++ blog/models.py
@@ -0,0 +1,57 @@
+from django.db import models
+from django.utils import timezone
+from django.conf import settings
+
+# Create your models here.
+class PublisedManager(models.Manager):
+
+    def get_queryset(self):
+        return (
+            super().get_queryset().filter(status=Post.Status.PUBLISHED)
+        )
+
+class Post(models.Model):
+
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,# using CASCADE to delete posts if user is deleted
+        related_name='blog_posts'
+    )
+    body = models.TextField()
+
+    published_date = models.DateTimeField(default=timezone.now)
+
+    '''
+    auto_now - updates the value of field to current time and date every time the model.save() is called.
+
+    auto_now_add - updates the value with the time and date of creation of record.
+    '''
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    status = models.CharField(
+        max_length=2,
+        choices=Status.choices,
+        default=Status.DRAFT
+    )
+    objects = models.Manager() # The default manager.
+    published = PublisedManager() # Our custom manager.
+
+    class Meta:
+        ordering = ['-published_date']
+        indexes = [
+            models.Index(fields=['-published_date']),
+        ]
+
+    def __str__(self):
+        return self.title
+    
+    
+    
+
